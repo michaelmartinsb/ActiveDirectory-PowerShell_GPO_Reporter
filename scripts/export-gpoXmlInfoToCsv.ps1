d1c12bd48dd8e7f4623ad52faed78092f7871918
Michael Martins-Baptista <144259566+michaelmartinsb@users.noreply.github.com>
2024-04-04 15:06:38 -0400

    Added Fields: Introduced additional fields to the GPO export script including ReadTime, CommentsIncluded, OwnerSID, Group, GroupSID, and extensions to the Permissions object to capture ApplicabilityToSelf, ApplicabilityToDescendantObjects, and ApplicabilityToDescendantContainers. This expansion provides a more detailed and comprehensive view of the GPO data. Configuration Flags: Added flags for ComputerConfigurationEnabled and UserConfigurationEnabled to indicate whether computer and user configurations are enabled within the GPO, enhancing the script's capability to differentiate GPO application scopes. Link Details: Expanded link details to include LinkPath and a boolean for NoOverride, offering a fuller understanding of the GPO's application environment and its hierarchical enforcement properties.

--- scripts/export-gpoXmlInfoToCsv.ps1
+++ scripts/export-gpoXmlInfoToCsv.ps1
@@ -1,24 +1,36 @@
 # Load the XML content from a file
 [xml]$gpoXml = Get-Content -Path "C:\temp\gpoAnalysis\GPOReports\xml\Global - Software Installation - Microsoft .NET SDK 6.0.418 (x64).xml"
 
-# Extracting key information
+# Extracting key information with added detail
 $gpoName = $gpoXml.GPO.Name
 $domain = $gpoXml.GPO.Identifier.Domain
 $gpoId = $gpoXml.GPO.Identifier.Identifier
 $createdTime = $gpoXml.GPO.CreatedTime
 $modifiedTime = $gpoXml.GPO.ModifiedTime
+$readTime = $gpoXml.GPO.ReadTime
+$commentsIncluded = $gpoXml.GPO.IncludeComments
 $securityDescriptor = $gpoXml.GPO.SecurityDescriptor.SDDL
 $owner = $gpoXml.GPO.SecurityDescriptor.Owner.Name
+$ownerSID = $gpoXml.GPO.SecurityDescriptor.Owner.SID
+$group = $gpoXml.GPO.SecurityDescriptor.Group.Name
+$groupSID = $gpoXml.GPO.SecurityDescriptor.Group.SID
 $permissions = $gpoXml.GPO.SecurityDescriptor.Permissions.TrusteePermissions | ForEach-Object {
     @{
         "Trustee" = $_.Trustee.Name
         "PermissionType" = $_.Type.PermissionType
         "Standard" = $_.Standard.GPOGroupedAccessEnum
+        "ApplicabilityToSelf" = $_.Applicability.ToSelf
+        "ApplicabilityToDescendantObjects" = $_.Applicability.ToDescendantObjects
+        "ApplicabilityToDescendantContainers" = $_.Applicability.ToDescendantContainers
     }
 }
-$linkTo = $gpoXml.GPO.LinksTo.SOMName
+$computerEnabled = $gpoXml.GPO.Computer.Enabled
 $scriptCommand = $gpoXml.GPO.Computer.ExtensionData.Extension.Script.Command
 $scriptType = $gpoXml.GPO.Computer.ExtensionData.Extension.Script.Type
+$userEnabled = $gpoXml.GPO.User.Enabled
+$linkTo = $gpoXml.GPO.LinksTo.SOMName
+$linkPath = $gpoXml.GPO.LinksTo.SOMPath
+$noOverride = $gpoXml.GPO.LinksTo.NoOverride
 
 # Create an object for each GPO to export to CSV
 $gpoObject = [PSCustomObject]@{
@@ -27,13 +39,25 @@ $gpoObject = [PSCustomObject]@{
     GPOId = $gpoId
     CreatedTime = $createdTime
     ModifiedTime = $modifiedTime
+    ReadTime = $readTime
+    CommentsIncluded = $commentsIncluded
     SecurityDescriptor = $securityDescriptor
     Owner = $owner
+    OwnerSID = $ownerSID
+    Group = $group
+    GroupSID = $groupSID
     Permissions = ($permissions | ConvertTo-Json -Compress)
-    LinkedTo = $linkTo
+    ComputerConfigurationEnabled = $computerEnabled
     ScriptCommand = $scriptCommand
     ScriptType = $scriptType
+    UserConfigurationEnabled = $userEnabled
+    LinkedTo = $linkTo
+    LinkPath = $linkPath
+    NoOverride = $noOverride
 }
 
+# Specify the path to export the CSV file
+$exportPath = "C:\temp\gpoAnalysis\GPOReports\GPOInformation2.csv"
+
 # Export to CSV
-$gpoObject | Export-Csv -Path "C:\temp\gpoAnalysis\GPOReports\GPOInformation.csv" -NoTypeInformation
+$gpoObject | Export-Csv -Path $exportPath -NoTypeInformation
